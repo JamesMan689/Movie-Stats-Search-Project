337c3fb13b384a80eb4f2bae74200b0a7e8a0f47
JamesMan689 <jameszxu20@gmail.com>
2023-07-25 02:25:30 -0400

    Updating to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/Movie-Stats-Search-Project/",
+  plugins: [react()],
 })
